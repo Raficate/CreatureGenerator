5f3fe585fd675bb79e9763ac595b0810eabd05f1
rafael.perez <r.perez@anro.es>
2025-02-18 13:29:43 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'creaturecreator',
+  webDir: 'www'
+};
+
+export default config;
